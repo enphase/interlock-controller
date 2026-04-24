2cfd30af3e097e703e7f01b60ec80b7345f1a069
ducky <richardlin@enphaseenergy.com>
2026-04-24 11:27:31 -0700

    Update tslot_nut.py

--- tslot_nut.py
+++ tslot_nut.py
@@ -33,12 +33,13 @@ class TSlotProfile(NamedTuple):
         return self.track_width / 2 - self.track_bottom_halfwidth()
 
 
+# from https://www.onlinemetals.com/en/buy/aluminum/45mm-x-45mm-light-w-10mm-slot-aluminum-t-slot-6063/pid/mp-00001401
 PROFILE_4545 = TSlotProfile(
     overall_width=45.0,
     slot_width=10.0,
     slot_depth=6.0,
-    track_width=20.0,
-    track_depth=13.0,
+    track_width=20.1,
+    track_depth=14.5,
     arm_thickness=3.0,
 )
 
@@ -46,12 +47,11 @@ PROFILE_4545 = TSlotProfile(
 def build_tslot_nut(
     profile: TSlotProfile = PROFILE_4545,
     nut: MetricNut = M4_NUT,
-    length: float = 25.0,
     clearance: float = 0.2,
     nut_depth_past_neck: float = 2.0,  # effectively wall thickness
     screw_entry_chamfer: float = 0.5,
     spring_thickness: float = 2.5,
-    spring_height: float = 6.0,
+    spring_height: float = 5.0,
     spring_interference: float = 1.5,
 ) -> cq.Workplane:
     """Build a T-slot nut for mounting into T-slot extrusion profiles.
@@ -71,6 +71,15 @@ def build_tslot_nut(
         CadQuery workplane with the T-slot nut body
     """
     INTERFERENCE_NUB_ANGLE_DEG = 30
+    nut_center_z = (
+        spring_height
+        + clearance * 2
+        + nut_depth_past_neck
+        + nut.across_flats / 2 * math.sqrt(3) / 2
+    )
+    length = (
+        nut_center_z + nut.across_flats / 2 * math.sqrt(3) / 2 + nut_depth_past_neck
+    )
 
     # Calculate dimensions with clearance
     neck_w = profile.slot_width - clearance * 2
@@ -109,8 +118,7 @@ def build_tslot_nut(
         .mirror(mirrorPlane="YZ", union=True)
     )
 
-    # Add hex nut cutout at Y=0 face, halfway up in Z
-    nut_locs = [(0, length / 2)]
+    nut_locs = [(0, nut_center_z)]
     body = apply_hex_nut_tool(
         wp=body.faces("<Y").workplane(),
         locations=nut_locs,
@@ -123,6 +131,25 @@ def build_tslot_nut(
     spring_y_end = profile.track_depth - clearance - clearance
     spring_y_start = profile.track_depth - clearance - clearance - spring_thickness
     spring_chamfer = chamfer  # account for truncated bottom from clearance
+
+    spring_base = (
+        cq.Workplane("XY")
+        .polyline(
+            [
+                # Start at the edge of the spring
+                (spring_height / 2 + spring_thickness, flange_y_start),
+                # Right side of flange
+                (flange_w / 2, flange_y_start),
+                (flange_w / 2, spring_y_end - spring_chamfer),
+                (flange_w / 2 - spring_chamfer, spring_y_end),
+                # Back to start
+                (spring_height / 2 + spring_thickness, spring_y_end),
+            ]
+        )
+        .close()
+        .extrude(spring_height)
+    )
+    body = body.union(spring_base)
 
     spring_recess = (
         cq.Workplane("XY")
@@ -144,25 +171,6 @@ def build_tslot_nut(
         .extrude(spring_height + clearance * 2)
     )
     body = body.cut(spring_recess)
-
-    spring_base = (
-        cq.Workplane("XY")
-        .polyline(
-            [
-                # Start at the edge of the spring
-                (spring_height / 2 + spring_thickness, flange_y_start),
-                # Right side of flange
-                (flange_w / 2, flange_y_start),
-                (flange_w / 2, spring_y_end - spring_chamfer),
-                (flange_w / 2 - spring_chamfer, spring_y_end),
-                # Back to start
-                (spring_height / 2 + spring_thickness, spring_y_end),
-            ]
-        )
-        .close()
-        .extrude(spring_height)
-    )
-    body = body.union(spring_base)
 
     spring_arm = (
         cq.Workplane("XY")
